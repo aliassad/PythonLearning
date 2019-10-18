bbd38a67dd70bfd3eebb46f4162cfc43b31b61f5
Ali.Assad <ali@A002-00012.arbisoft.com>
2019-10-18 17:27:13 +0500

    code containg the octoboer training projects

--- Django-Applications/Django-Blog/django_project/users/views.py
+++ Django-Applications/Django-Blog/django_project/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm
 
 
@@ -14,3 +15,7 @@ def register(request):
     else:
         form = UserRegistrationForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+def profile(request):
+    return render(request, 'users/profile.html')
